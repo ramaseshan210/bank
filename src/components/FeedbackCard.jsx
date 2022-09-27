507424a6397358d0dde0b186f4e04c47b5323053
Ram <ramannp210@gmail.com>
2022-09-27 12:33:11 +0530

    init vite project

---
+++ src/components/FeedbackCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {twitter  } from '../assets'
+
+const FeedbackCard = ({content ,name,title ,image}) => (
+  <div className='flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card'>
+ 
+  </div>
+)
+
+export default FeedbackCard
---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,14 @@
+import { feedback } from "../constants";
+import styles from "../style";
+import CardDeal from "./CardDeal";
+import FeedbackCard from "./FeedbackCard";
+
+const Testimonials = () => {
+  return (
+    <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
+      
+    </section>
+  )
+}
+
+export default Testimonials
